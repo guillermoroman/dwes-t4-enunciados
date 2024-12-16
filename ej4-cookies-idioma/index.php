bce7223ec4bdda0311553c6b651f6aa9a91e76ab
guillermoroman <guillermoroman@outlook.com>
2024-12-16 13:20:19 +0100

    ejercicio 4

--- ej4-cookies-idioma/index.php
+++ ej4-cookies-idioma/index.php
@@ -1,3 +1,15 @@
+<?php
+$idioma = 'es';
+
+if (isset($_POST["idioma"])){
+    $idioma = $_POST["idioma"];
+    setcookie("idioma", $idioma, time() + 35600*24);
+} else {
+    $idioma = $_COOKIE["idioma"];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +18,11 @@
     <title>Document</title>
 </head>
 <body>
+    <h1><?php echo $idioma == 'es' ? 'Bienvenido' : 'Welcome' ?></h1>
     
+    <form method="POST">
+        <button name = "idioma" value ="es">Español</button>
+        <button name = "idioma" value ="en">English</button>
+    </form>
 </body>
 </html>
